7bf0314927f8aaf356c6d931c372c48b8b26766c
Kevin T <47423873+8Ten10@users.noreply.github.com>
2024-10-07 23:14:42 -0400

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -2,5 +2,6 @@
 
 sudo sed -i -re 's/([a-z]{2}.)?archive.ubuntu.com|security.ubuntu.com/old-releases.ubuntu.com/g' /etc/apt/sources.list
 sudo apt update
+sudo apt install openssh-server
 sudo apt install git
 sudo apt install make
